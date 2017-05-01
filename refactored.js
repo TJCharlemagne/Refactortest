d76e8ef541a43845ca64e27422d098400ff3d1ee
Vintcient <tjaustin1890@gmail.com>
2017-05-01 10:23:07 -0400

    Refactored code using for loop to create 15 randomly colored spheres.

---
+++ refactored.js
@@ -0,0 +1,14 @@
+var html = '';
+var red;
+var green;
+var blue;
+var rgbColor;
+
+for (var colorCount = 0; colorCount < 15; colorCount += 1){
+red = Math.floor(Math.random() * 256 );
+green = Math.floor(Math.random() * 256 );
+blue = Math.floor(Math.random() * 256 );
+rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+html += '<div style="background-color:' + rgbColor + '"></div>';}
+
+document.write(html);
